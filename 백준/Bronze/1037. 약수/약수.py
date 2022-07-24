19066c05379882d1e0bda7d3900682fdfb9e7a13
anjiwon319 <66212424+anjiwon319@users.noreply.github.com>
2022-07-24 19:27:21 +0900

    [Bronze I] Title: 약수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.py
+++ 백준/Bronze/1037. 약수/약수.py
@@ -2,8 +2,7 @@ len = int(input())
 divisior = input().split(' ')
 
 # divisior 리스트 내의 값을 int로 타입 변경 
-for i in range(len):
-    divisior[i] = int(divisior[i])
+divisior = list(map(int, divisior))
 
 # 약수들을 오름차순으로 정렬
 divisior.sort()
