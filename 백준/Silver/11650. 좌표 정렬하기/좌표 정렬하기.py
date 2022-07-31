ddcaf2de31d6f2ea46e7e7731a9d42f074b4010b
anjiwon319 <66212424+anjiwon319@users.noreply.github.com>
2022-08-01 02:36:12 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 4252 ms, Memory: 45472 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -7,8 +7,9 @@ for i in range(len):
     x, y = map(int, input().split())
     dots.append((x, y))
 
-# sort로 다중 조건 정렬해서 출력 (x 우선 정렬 후 y 정렬)
-dots.sort(key = lambda x : (x[0], x[1]))
+# sort 사용해서 y 먼저 정렬 후, x 정렬 -> 방법2
+dots.sort(key = lambda x : x[1])
+dots.sort(key = lambda x : x[0])
 
 # 출력 형식에 맞게 출력
 for i in range(len):
