c05af7793f0f220dd683cebac6b306384457b03a
anjiwon319 <66212424+anjiwon319@users.noreply.github.com>
2022-08-03 01:45:27 +0900

    [Bronze III] Title: 직각삼각형, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,23 @@
+# 직각삼각형 (a right triangle)
+import sys
+# 빠른 입력을 위한 코드
+input = sys.stdin.readline
+
+flag = True
+legs = []
+# 입력값 받기
+while flag:
+    legs.append(list(map(int, input().split())))
+    if legs[-1][0] == 0:
+        flag = False
+        del legs[-1]
+
+for i in legs:
+    a = sorted(i)[0]
+    b = sorted(i)[1]
+    c = sorted(i)[2]
+    # 직각 삼각형 확인 조건 : 가장 큰 변(c)의 제곱이 나머지 변(a, b)의 제곱합과 같다
+    if a**2 + b**2 == c**2:
+        print("right")
+    else:
+        print("wrong")
