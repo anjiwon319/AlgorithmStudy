1e8daf952dd88ca1073e32c1c5f1949def25811f
anjiwon319 <66212424+anjiwon319@users.noreply.github.com>
2022-08-09 03:12:01 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 392 ms, Memory: 45472 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/README.md
@@ -0,0 +1,12 @@
+# [Silver V] 좌표 정렬하기 2 - 11651 
+
+[문제 링크](https://www.acmicpc.net/problem/11651) 
+
+### 성능 요약
+
+메모리: 45472 KB, 시간: 392 ms
+
+### 분류
+
+정렬(sorting)
+
---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,18 @@
+# 좌표 정렬하기
+import sys
+input = sys.stdin.readline
+
+len = int(input())
+dots = []
+
+for i in range(len):
+    x, y = map(int, input().split())
+    dots.append((x, y))
+
+# sort 사용해서 y 먼저 정렬 후, x 정렬 -> 방법2
+dots.sort(key = lambda x : x[0])
+dots.sort(key = lambda x : x[1])
+
+# 출력 형식에 맞게 출력
+for i in range(len):
+    print(f"{dots[i][0]} {dots[i][1]}")
