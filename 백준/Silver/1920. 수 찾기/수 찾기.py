5c5e15fd6d56f08aeefccdd69e1edbd25d15e78b
anjiwon319 <66212424+anjiwon319@users.noreply.github.com>
2022-08-26 15:42:54 +0900

    [Silver IV] Title: 수 찾기, Time: 200 ms, Memory: 50092 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -10,8 +10,8 @@ a_set = set(map(int, input().split()))
 m = int(input())
 m_list = list(map(int, input().split()))
 
-for i in range(m):
-    if m_list[i] in a_set:
+for i in m_list:
+    if i in a_set:
         print(1)
     else:
         print(0)
