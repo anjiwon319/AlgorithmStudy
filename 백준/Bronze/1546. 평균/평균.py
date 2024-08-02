94de965673f0605fcc44f815598e8b0983d64bac
JiWon An <66212424+anjiwon319@users.noreply.github.com>
2024-08-02 14:50:21 -0700

    [Bronze I] Title: 평균, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,9 +1,6 @@
 n = int(input())
 scores = list(map(int, input().split()))
 M = max(scores)
-
-for i in range(n):
-    scores[i] = scores[i] / M
-
+scores = [score/M for score in scores]
 mean = sum(scores) / n * 100
 print(mean)
