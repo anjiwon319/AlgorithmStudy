404d7d5df1ef48237520a599607791701b389189
JiWon An <66212424+anjiwon319@users.noreply.github.com>
2026-01-25 14:43:25 +0900

    [level 3] Title: 단속카메라, Time: 0.83 ms, Memory: 9.59 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,5 +1,5 @@
 def solution(routes):
-    routes.sort(key= lambda x: x[0:1])
+    routes.sort(key= lambda x: x[1])
     
     camera = -30001
     answer = 0
@@ -7,8 +7,6 @@ def solution(routes):
     for start, end in routes:
         if start > camera:
             answer += 1
-            camera = end
-        if end < camera:
             camera = end
         
     return answer
